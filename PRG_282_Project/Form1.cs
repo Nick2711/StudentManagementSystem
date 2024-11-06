4decf1602895de98044650613821fc9256d2bbe4
DeanMeiring <meiringdean2409@gmail.com>
2024-11-06 13:16:48 +0200

    Code add
    Add code

---
+++ PRG_282_Project/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG_282_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Meow");
+
+             
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Helloo villlyy");
+
+            MessageBox.Show("Hi there");
+            MessageBox.Show("Are you sure you want to");
+
+        }
+
+
+        }
+
+    }
+}
