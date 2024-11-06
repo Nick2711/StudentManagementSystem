aa6d41f93a97a71caefd15b2a9e261d836385dea
Cimannin <viljoensteenkamp2004@gmail.com>
2024-11-06 13:34:20 +0200

    Added Front-end
    Multiple forms for each button on the home page were added, adding some colour and a button to exit the application.
    
    back-end must be done for adequate usage of each button and form.

--- PRG_282_Project/Form1.Designer.cs
+++ PRG_282_Project/Form1.Designer.cs
@@ -28,72 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.Exitbtn = new System.Windows.Forms.Button();
+            this.GenerateSummarybtn = new System.Windows.Forms.Button();
+            this.panelMediaSubmenu = new System.Windows.Forms.Panel();
+            this.DeleteStudentNavbtn = new System.Windows.Forms.Button();
+            this.UpdateDetailsNavbtn = new System.Windows.Forms.Button();
+            this.ViewStudentsNavbtn = new System.Windows.Forms.Button();
+            this.AddStudentNavbtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.DisplayInfoPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelSideMenu.SuspendLayout();
+            this.panelMediaSubmenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.DisplayInfoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // panelSideMenu
             // 
-
-
-            this.button1.Location = new System.Drawing.Point(464, 173);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 60);
-
-
-            this.button1.Location = new System.Drawing.Point(431, 125);
-
-            this.button1.Location = new System.Drawing.Point(585, 253);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 49);
-
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.DarkGray;
+            this.panelSideMenu.Controls.Add(this.Exitbtn);
+            this.panelSideMenu.Controls.Add(this.GenerateSummarybtn);
+            this.panelSideMenu.Controls.Add(this.panelMediaSubmenu);
+            this.panelSideMenu.Controls.Add(this.panel2);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(217, 580);
+            this.panelSideMenu.TabIndex = 0;
             // 
-            // button2
+            // Exitbtn
             // 
-            this.button2.Location = new System.Drawing.Point(351, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 52);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Exitbtn.BackColor = System.Drawing.Color.Gray;
+            this.Exitbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Exitbtn.FlatAppearance.BorderSize = 0;
+            this.Exitbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exitbtn.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exitbtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Exitbtn.Location = new System.Drawing.Point(0, 446);
+            this.Exitbtn.Name = "Exitbtn";
+            this.Exitbtn.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.Exitbtn.Size = new System.Drawing.Size(217, 57);
+            this.Exitbtn.TabIndex = 6;
+            this.Exitbtn.Text = "Exit";
+            this.Exitbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Exitbtn.UseVisualStyleBackColor = false;
+            this.Exitbtn.Click += new System.EventHandler(this.Exitbtn_Click);
+            // 
+            // GenerateSummarybtn
+            // 
+            this.GenerateSummarybtn.BackColor = System.Drawing.Color.Gray;
+            this.GenerateSummarybtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GenerateSummarybtn.FlatAppearance.BorderSize = 0;
+            this.GenerateSummarybtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenerateSummarybtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.GenerateSummarybtn.Location = new System.Drawing.Point(0, 390);
+            this.GenerateSummarybtn.Name = "GenerateSummarybtn";
+            this.GenerateSummarybtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.GenerateSummarybtn.Size = new System.Drawing.Size(217, 56);
+            this.GenerateSummarybtn.TabIndex = 5;
+            this.GenerateSummarybtn.Text = "Generate Summary";
+            this.GenerateSummarybtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GenerateSummarybtn.UseVisualStyleBackColor = false;
+            this.GenerateSummarybtn.Click += new System.EventHandler(this.GenerateSummarybtn_Click);
+            // 
+            // panelMediaSubmenu
+            // 
+            this.panelMediaSubmenu.Controls.Add(this.DeleteStudentNavbtn);
+            this.panelMediaSubmenu.Controls.Add(this.UpdateDetailsNavbtn);
+            this.panelMediaSubmenu.Controls.Add(this.ViewStudentsNavbtn);
+            this.panelMediaSubmenu.Controls.Add(this.AddStudentNavbtn);
+            this.panelMediaSubmenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMediaSubmenu.Location = new System.Drawing.Point(0, 174);
+            this.panelMediaSubmenu.Name = "panelMediaSubmenu";
+            this.panelMediaSubmenu.Size = new System.Drawing.Size(217, 216);
+            this.panelMediaSubmenu.TabIndex = 1;
+            // 
+            // DeleteStudentNavbtn
+            // 
+            this.DeleteStudentNavbtn.BackColor = System.Drawing.Color.Gray;
+            this.DeleteStudentNavbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DeleteStudentNavbtn.FlatAppearance.BorderSize = 0;
+            this.DeleteStudentNavbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteStudentNavbtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.DeleteStudentNavbtn.Location = new System.Drawing.Point(0, 159);
+            this.DeleteStudentNavbtn.Name = "DeleteStudentNavbtn";
+            this.DeleteStudentNavbtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.DeleteStudentNavbtn.Size = new System.Drawing.Size(217, 57);
+            this.DeleteStudentNavbtn.TabIndex = 4;
+            this.DeleteStudentNavbtn.Text = "Delete Student";
+            this.DeleteStudentNavbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.DeleteStudentNavbtn.UseVisualStyleBackColor = false;
+            this.DeleteStudentNavbtn.Click += new System.EventHandler(this.DeleteStudentNavbtn_Click);
+            // 
+            // UpdateDetailsNavbtn
+            // 
+            this.UpdateDetailsNavbtn.BackColor = System.Drawing.Color.Gray;
+            this.UpdateDetailsNavbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UpdateDetailsNavbtn.FlatAppearance.BorderSize = 0;
+            this.UpdateDetailsNavbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateDetailsNavbtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.UpdateDetailsNavbtn.Location = new System.Drawing.Point(0, 104);
+            this.UpdateDetailsNavbtn.Name = "UpdateDetailsNavbtn";
+            this.UpdateDetailsNavbtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.UpdateDetailsNavbtn.Size = new System.Drawing.Size(217, 55);
+            this.UpdateDetailsNavbtn.TabIndex = 3;
+            this.UpdateDetailsNavbtn.Text = "Update Details";
+            this.UpdateDetailsNavbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.UpdateDetailsNavbtn.UseVisualStyleBackColor = false;
+            this.UpdateDetailsNavbtn.Click += new System.EventHandler(this.UpdateDetailsNavbtn_Click);
+            // 
+            // ViewStudentsNavbtn
+            // 
+            this.ViewStudentsNavbtn.BackColor = System.Drawing.Color.Gray;
+            this.ViewStudentsNavbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ViewStudentsNavbtn.FlatAppearance.BorderSize = 0;
+            this.ViewStudentsNavbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ViewStudentsNavbtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.ViewStudentsNavbtn.Location = new System.Drawing.Point(0, 51);
+            this.ViewStudentsNavbtn.Name = "ViewStudentsNavbtn";
+            this.ViewStudentsNavbtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.ViewStudentsNavbtn.Size = new System.Drawing.Size(217, 53);
+            this.ViewStudentsNavbtn.TabIndex = 2;
+            this.ViewStudentsNavbtn.Text = "View Students";
+            this.ViewStudentsNavbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ViewStudentsNavbtn.UseVisualStyleBackColor = false;
+            this.ViewStudentsNavbtn.Click += new System.EventHandler(this.ViewStudentsNavbtn_Click);
+            // 
+            // AddStudentNavbtn
+            // 
+            this.AddStudentNavbtn.BackColor = System.Drawing.Color.Gray;
+            this.AddStudentNavbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddStudentNavbtn.FlatAppearance.BorderSize = 0;
+            this.AddStudentNavbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddStudentNavbtn.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.AddStudentNavbtn.Location = new System.Drawing.Point(0, 0);
+            this.AddStudentNavbtn.Name = "AddStudentNavbtn";
+            this.AddStudentNavbtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.AddStudentNavbtn.Size = new System.Drawing.Size(217, 51);
+            this.AddStudentNavbtn.TabIndex = 1;
+            this.AddStudentNavbtn.Text = "Add Student";
+            this.AddStudentNavbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddStudentNavbtn.UseVisualStyleBackColor = false;
+            this.AddStudentNavbtn.Click += new System.EventHandler(this.AddStudentNavbtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnOptions);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(217, 174);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.BackColor = System.Drawing.Color.Gray;
+            this.btnOptions.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnOptions.FlatAppearance.BorderSize = 0;
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOptions.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnOptions.Location = new System.Drawing.Point(0, 116);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnOptions.Size = new System.Drawing.Size(217, 58);
+            this.btnOptions.TabIndex = 0;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOptions.UseVisualStyleBackColor = false;
+            // 
+            // DisplayInfoPanel
+            // 
+            this.DisplayInfoPanel.Controls.Add(this.pictureBox1);
+            this.DisplayInfoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DisplayInfoPanel.Location = new System.Drawing.Point(217, 0);
+            this.DisplayInfoPanel.Name = "DisplayInfoPanel";
+            this.DisplayInfoPanel.Size = new System.Drawing.Size(714, 580);
+            this.DisplayInfoPanel.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(221, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(441, 231);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-
-            this.Margin = new System.Windows.Forms.Padding(4);
-
-
-
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(931, 580);
+            this.Controls.Add(this.DisplayInfoPanel);
+            this.Controls.Add(this.panelSideMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Home Page";
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelMediaSubmenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.DisplayInfoPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Panel panelMediaSubmenu;
+        private System.Windows.Forms.Button UpdateDetailsNavbtn;
+        private System.Windows.Forms.Button ViewStudentsNavbtn;
+        private System.Windows.Forms.Button AddStudentNavbtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button DeleteStudentNavbtn;
+        private System.Windows.Forms.Button GenerateSummarybtn;
+        private System.Windows.Forms.Button Exitbtn;
+        private System.Windows.Forms.Panel DisplayInfoPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
