2d9f5b1867caf85caec79e961156d07ec961db16
Nick2711 <Nicholas2711kelly@gmail.com>
2024-11-10 00:52:22 +0200

    Added Generate summary code

--- PRG_282_Project/Summary.Designer.cs
+++ PRG_282_Project/Summary.Designer.cs
@@ -28,57 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.GenerateSumbtn = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.summaryRichTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(339, 273);
-            this.panel1.TabIndex = 0;
             // 
             // GenerateSumbtn
             // 
-            this.GenerateSumbtn.Location = new System.Drawing.Point(259, 291);
+            this.GenerateSumbtn.Location = new System.Drawing.Point(200, 340);
+            this.GenerateSumbtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.GenerateSumbtn.Name = "GenerateSumbtn";
-            this.GenerateSumbtn.Size = new System.Drawing.Size(204, 52);
+            this.GenerateSumbtn.Size = new System.Drawing.Size(153, 42);
             this.GenerateSumbtn.TabIndex = 1;
             this.GenerateSumbtn.Text = "Generate Summary";
             this.GenerateSumbtn.UseVisualStyleBackColor = true;
+            this.GenerateSumbtn.Click += new System.EventHandler(this.GenerateSumbtn_Click);
             // 
-            // dataGridView1
+            // summaryRichTextBox
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(363, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(339, 273);
-            this.dataGridView1.TabIndex = 2;
+            this.summaryRichTextBox.Location = new System.Drawing.Point(12, 31);
+            this.summaryRichTextBox.Name = "summaryRichTextBox";
+            this.summaryRichTextBox.Size = new System.Drawing.Size(512, 304);
+            this.summaryRichTextBox.TabIndex = 2;
+            this.summaryRichTextBox.Text = "";
             // 
             // Summary
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 580);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(536, 471);
+            this.Controls.Add(this.summaryRichTextBox);
             this.Controls.Add(this.GenerateSumbtn);
-            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Summary";
             this.Text = "Summary";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button GenerateSumbtn;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox summaryRichTextBox;
     }
 }
--- PRG_282_Project/Summary.cs
+++ PRG_282_Project/Summary.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,118 @@ namespace PRG_282_Project
         public Summary()
         {
             InitializeComponent();
+        }
+        private static readonly HttpClient httpClient = new HttpClient();
+        private const string StudentsFileUrl = "https://raw.githubusercontent.com/Nick2711/StudentManagementSystem/main/PRG_282_Project/Students.txt";
+        private const string SummaryFileApiUrl = "https://api.github.com/repos/Nick2711/StudentManagementSystem/contents/PRG_282_Project/Summary.txt";
+
+
+
+        private async void GenerateSumbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string studentData = await FetchFileFromGitHub(StudentsFileUrl);
+                if (studentData == null)
+                {
+                    MessageBox.Show("Failed to retrieve student data.");
+                    return;
+                }
+
+                
+                var studentRecords = studentData.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                int totalStudents = studentRecords.Length;
+                double averageAge = studentRecords
+                    .Select(record => record.Split(','))
+                    .Where(data => data.Length == 4 && int.TryParse(data[2], out _))
+                    .Select(data => int.Parse(data[2]))
+                    .Average();
+
+                
+                string summaryText = $"Student Summary Report\n\n" +
+                                     $"Total Students: {totalStudents}\n" +
+                                     $"Average Age: {averageAge:F2}\n";
+
+                
+                summaryRichTextBox.Clear();
+                summaryRichTextBox.AppendText(summaryText);
+
+                
+                bool isSaved = await SaveSummaryToGitHub(summaryText);
+                if (isSaved)
+                {
+                    MessageBox.Show("Summary saved successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+        }
+
+
+        private async Task<string> FetchFileFromGitHub(string fileUrl)
+        {
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("MyApp");
+            HttpResponseMessage response = await httpClient.GetAsync(fileUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            return null;
+        }
+
+        private async Task<string> GetFileSha()
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("GITHUB_TOKEN"));
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("MyApp");
+
+            HttpResponseMessage response = await httpClient.GetAsync(SummaryFileApiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                dynamic jsonData = JsonConvert.DeserializeObject(jsonResponse);
+                return jsonData.sha;
+            }
+
+            return null;
+        }
+
+        private async Task<bool> SaveSummaryToGitHub(string content)
+        {
+            try
+            {
+                
+                string base64Content = Convert.ToBase64String(Encoding.UTF8.GetBytes(content));
+
+                
+                string fileSha = await GetFileSha();
+
+                
+                var payload = new
+                {
+                    message = "Update summary report",
+                    content = base64Content,
+                    sha = fileSha
+                };
+
+                
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("GITHUB_TOKEN"));
+                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("MyApp");
+
+                
+                var requestContent = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await httpClient.PutAsync(SummaryFileApiUrl, requestContent);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while saving the summary: {ex.Message}");
+                return false;
+            }
         }
     }
 }
