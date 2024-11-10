6a7fbec768607d2a9a85591687f5a12dbfc25eed
Cimannin <viljoensteenkamp2004@gmail.com>
2024-11-10 18:51:01 +0200

    Updated
    Yes

--- PRG_282_Project/AddStudent.Designer.cs
+++ PRG_282_Project/AddStudent.Designer.cs
@@ -45,56 +45,64 @@
             // 
             // StudentID
             // 
-            this.StudentID.Location = new System.Drawing.Point(244, 103);
+            this.StudentID.Location = new System.Drawing.Point(325, 127);
+            this.StudentID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StudentID.Name = "StudentID";
-            this.StudentID.Size = new System.Drawing.Size(189, 20);
+            this.StudentID.Size = new System.Drawing.Size(251, 22);
             this.StudentID.TabIndex = 0;
             this.StudentID.TextChanged += new System.EventHandler(this.StudentID_TextChanged);
             // 
             // Age
             // 
-            this.Age.Location = new System.Drawing.Point(244, 255);
+            this.Age.Location = new System.Drawing.Point(325, 314);
+            this.Age.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Age.Name = "Age";
-            this.Age.Size = new System.Drawing.Size(189, 20);
+            this.Age.Size = new System.Drawing.Size(251, 22);
             this.Age.TabIndex = 1;
             this.Age.TextChanged += new System.EventHandler(this.Age_TextChanged);
             // 
             // Name
             // 
-            this.Name.Location = new System.Drawing.Point(244, 174);
+            this.Name.Location = new System.Drawing.Point(325, 214);
+            this.Name.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name.Name = "Name";
-            this.Name.Size = new System.Drawing.Size(189, 20);
+            this.Name.Size = new System.Drawing.Size(251, 22);
             this.Name.TabIndex = 2;
             this.Name.TextChanged += new System.EventHandler(this.Name_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(244, 103);
+            this.textBox1.Location = new System.Drawing.Point(325, 127);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.Size = new System.Drawing.Size(251, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(244, 255);
+            this.textBox2.Location = new System.Drawing.Point(325, 314);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(189, 20);
+            this.textBox2.Size = new System.Drawing.Size(251, 22);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(244, 174);
+            this.textBox3.Location = new System.Drawing.Point(325, 214);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(189, 20);
+            this.textBox3.Size = new System.Drawing.Size(251, 22);
             this.textBox3.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(62, 103);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(139, 127);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.Size = new System.Drawing.Size(104, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Student ID:";
             // 
@@ -102,9 +110,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(104, 174);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(165, 214);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.Size = new System.Drawing.Size(63, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Name:";
             // 
@@ -112,9 +122,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(124, 255);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(181, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.Size = new System.Drawing.Size(47, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Age:";
             // 
@@ -122,35 +134,40 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(96, 331);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(153, 410);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.Size = new System.Drawing.Size(75, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Course:";
             // 
             // CourseBox
             // 
             this.CourseBox.FormattingEnabled = true;
-            this.CourseBox.Location = new System.Drawing.Point(244, 330);
+            this.CourseBox.Location = new System.Drawing.Point(325, 406);
+            this.CourseBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CourseBox.Name = "CourseBox";
-            this.CourseBox.Size = new System.Drawing.Size(189, 21);
+            this.CourseBox.Size = new System.Drawing.Size(251, 24);
             this.CourseBox.TabIndex = 8;
             this.CourseBox.SelectedIndexChanged += new System.EventHandler(this.CourseBox_SelectedIndexChanged);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(244, 330);
+            this.comboBox1.Location = new System.Drawing.Point(325, 406);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(189, 21);
+            this.comboBox1.Size = new System.Drawing.Size(251, 24);
             this.comboBox1.TabIndex = 8;
             // 
             // StudentSubmitbtn
             // 
             this.StudentSubmitbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StudentSubmitbtn.Location = new System.Drawing.Point(270, 390);
+            this.StudentSubmitbtn.Location = new System.Drawing.Point(360, 480);
+            this.StudentSubmitbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StudentSubmitbtn.Name = "StudentSubmitbtn";
-            this.StudentSubmitbtn.Size = new System.Drawing.Size(140, 49);
+            this.StudentSubmitbtn.Size = new System.Drawing.Size(187, 60);
             this.StudentSubmitbtn.TabIndex = 9;
             this.StudentSubmitbtn.Text = "Submit";
             this.StudentSubmitbtn.UseVisualStyleBackColor = true;
@@ -158,10 +175,10 @@
             // 
             // AddStudent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSlateGray;
-            this.ClientSize = new System.Drawing.Size(714, 580);
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(731, 577);
             this.Controls.Add(this.StudentSubmitbtn);
             this.Controls.Add(this.CourseBox);
             this.Controls.Add(this.comboBox1);
@@ -175,7 +192,8 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-           
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AddStudent";
             this.Load += new System.EventHandler(this.AddStudent_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
