62eb8877baf786d5ad851f0c7d40376c81f3b2e3
Cimannin <viljoensteenkamp2004@gmail.com>
2024-11-10 16:37:02 +0200

    Revert "Addstudents form"
    This reverts commit e9e9b817a5702aa94b2ad445f8d180af94b6143d.

--- PRG_282_Project/AddStudent.Designer.cs
+++ PRG_282_Project/AddStudent.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 =======
-=======
->>>>>>> Stashed changes
             this.StudentID = new System.Windows.Forms.TextBox();
             this.Age = new System.Windows.Forms.TextBox();
             this.Name = new System.Windows.Forms.TextBox();
@@ -45,13 +42,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.StudentSubmitbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
 =======
-=======
->>>>>>> Stashed changes
             this.CourseBox = new System.Windows.Forms.ComboBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.StudentSubmitbtn = new System.Windows.Forms.Button();
@@ -59,24 +53,15 @@
             // 
             // StudentID
             // 
-<<<<<<< Updated upstream
             this.StudentID.Location = new System.Drawing.Point(312, 92);
-=======
-            this.StudentID.Location = new System.Drawing.Point(311, 118);
->>>>>>> Stashed changes
             this.StudentID.Name = "StudentID";
             this.StudentID.Size = new System.Drawing.Size(189, 22);
             this.StudentID.TabIndex = 0;
             this.StudentID.TextChanged += new System.EventHandler(this.StudentID_TextChanged);
-            this.StudentID.Validating += new System.ComponentModel.CancelEventHandler(this.StudentID_Validating);
             // 
             // Age
             // 
-<<<<<<< Updated upstream
             this.Age.Location = new System.Drawing.Point(312, 244);
-=======
-            this.Age.Location = new System.Drawing.Point(311, 270);
->>>>>>> Stashed changes
             this.Age.Name = "Age";
             this.Age.Size = new System.Drawing.Size(189, 22);
             this.Age.TabIndex = 1;
@@ -84,50 +69,30 @@
             // 
             // Name
             // 
-<<<<<<< Updated upstream
             this.Name.Location = new System.Drawing.Point(312, 163);
-=======
-            this.Name.Location = new System.Drawing.Point(311, 189);
->>>>>>> Stashed changes
             this.Name.Name = "Name";
             this.Name.Size = new System.Drawing.Size(189, 22);
             this.Name.TabIndex = 2;
             this.Name.TextChanged += new System.EventHandler(this.Name_TextChanged);
-<<<<<<< Updated upstream
 >>>>>>> Stashed changes
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(312, 92);
-=======
-            this.Name.Validating += new System.ComponentModel.CancelEventHandler(this.Name_Validating);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(311, 118);
->>>>>>> Stashed changes
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(189, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-<<<<<<< Updated upstream
             this.textBox2.Location = new System.Drawing.Point(312, 244);
-=======
-            this.textBox2.Location = new System.Drawing.Point(311, 270);
->>>>>>> Stashed changes
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(189, 22);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-<<<<<<< Updated upstream
             this.textBox3.Location = new System.Drawing.Point(312, 163);
-=======
-            this.textBox3.Location = new System.Drawing.Point(311, 189);
->>>>>>> Stashed changes
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(189, 22);
             this.textBox3.TabIndex = 2;
@@ -136,13 +101,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< Updated upstream
             this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(134, 92);
-=======
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(129, 118);
->>>>>>> Stashed changes
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 23);
             this.label1.TabIndex = 4;
@@ -152,17 +112,11 @@
             // 
             this.label2.AutoSize = true;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             this.label2.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(171, 167);
 =======
             this.label2.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold);
             this.label2.Location = new System.Drawing.Point(172, 163);
->>>>>>> Stashed changes
-=======
-            this.label2.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(171, 189);
 >>>>>>> Stashed changes
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 23);
@@ -173,17 +127,11 @@
             // 
             this.label3.AutoSize = true;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             this.label3.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(191, 248);
 =======
             this.label3.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(192, 244);
->>>>>>> Stashed changes
-=======
-            this.label3.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(191, 270);
 >>>>>>> Stashed changes
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 23);
@@ -194,17 +142,11 @@
             // 
             this.label4.AutoSize = true;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             this.label4.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(163, 324);
 =======
             this.label4.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(164, 320);
->>>>>>> Stashed changes
-=======
-            this.label4.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(163, 346);
 >>>>>>> Stashed changes
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 23);
@@ -212,35 +154,21 @@
             this.label4.Text = "Course:";
             // 
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
 =======
             // CourseBox
             // 
             this.CourseBox.FormattingEnabled = true;
             this.CourseBox.Location = new System.Drawing.Point(312, 319);
-=======
-            // CourseBox
-            // 
-            this.CourseBox.FormattingEnabled = true;
-            this.CourseBox.Location = new System.Drawing.Point(311, 345);
->>>>>>> Stashed changes
             this.CourseBox.Name = "CourseBox";
             this.CourseBox.Size = new System.Drawing.Size(189, 24);
             this.CourseBox.TabIndex = 8;
             this.CourseBox.SelectedIndexChanged += new System.EventHandler(this.CourseBox_SelectedIndexChanged);
             // 
-<<<<<<< Updated upstream
 >>>>>>> Stashed changes
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(312, 319);
-=======
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(311, 345);
->>>>>>> Stashed changes
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(189, 24);
             this.comboBox1.TabIndex = 8;
@@ -249,14 +177,10 @@
             // 
 <<<<<<< Updated upstream
             this.StudentSubmitbtn.Font = new System.Drawing.Font("Rockwell Nova", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< Updated upstream
             this.StudentSubmitbtn.Location = new System.Drawing.Point(337, 383);
 =======
             this.StudentSubmitbtn.Font = new System.Drawing.Font("Rockwell Nova", 7.8F, System.Drawing.FontStyle.Bold);
             this.StudentSubmitbtn.Location = new System.Drawing.Point(338, 379);
->>>>>>> Stashed changes
-=======
-            this.StudentSubmitbtn.Location = new System.Drawing.Point(337, 405);
 >>>>>>> Stashed changes
             this.StudentSubmitbtn.Name = "StudentSubmitbtn";
             this.StudentSubmitbtn.Size = new System.Drawing.Size(140, 49);
@@ -264,10 +188,6 @@
             this.StudentSubmitbtn.Text = "Submit";
             this.StudentSubmitbtn.UseVisualStyleBackColor = true;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-            this.StudentSubmitbtn.Click += new System.EventHandler(this.StudentSubmitbtn_Click);
->>>>>>> Stashed changes
 =======
             this.StudentSubmitbtn.Click += new System.EventHandler(this.StudentSubmitbtn_Click);
 >>>>>>> Stashed changes
@@ -276,14 +196,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.BackColor = System.Drawing.SystemColors.Menu;
             this.ClientSize = new System.Drawing.Size(714, 580);
             this.Controls.Add(this.StudentSubmitbtn);
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-            this.Controls.Add(this.CourseBox);
->>>>>>> Stashed changes
 =======
             this.Controls.Add(this.CourseBox);
 >>>>>>> Stashed changes
@@ -293,15 +209,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "AddStudent";
             this.Text = "Form2";
 =======
-=======
->>>>>>> Stashed changes
             this.Controls.Add(this.Name);
             this.Controls.Add(this.Age);
             this.Controls.Add(this.StudentID);
@@ -311,9 +224,6 @@
             //this.Name = "AddStudent";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.AddStudent_Load);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
 >>>>>>> Stashed changes
             this.ResumeLayout(false);
             this.PerformLayout();
